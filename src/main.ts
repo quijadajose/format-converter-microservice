33ff7033e827c3d7332c1ff2b14a62aaeba9b908
Desconocido <quijadajosed@gmail.com>
2024-09-19 14:48:08 -0300

    configure NestJS microservice with RabbitMQ transport and queue settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+// }
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],  // URL de RabbitMQ (puedes cambiarla por tu config)
+      queue: 'video_conversion_queue',   // Nombre de la cola que estarás utilizando
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
+  await app.listen();
 }
 bootstrap();
