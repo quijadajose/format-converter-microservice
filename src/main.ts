933e9b97c7f7c07d46c2e173287010f61f15dd28
Desconocido <quijadajosed@gmail.com>
2024-09-19 14:49:55 -0300

    refactor main.ts to create a NestJS microservice with RabbitMQ transport

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   await app.listen(3000);
-// }
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.RMQ,
