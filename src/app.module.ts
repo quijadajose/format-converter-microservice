6f8633f68ecdd828e7424b8fa501889895c76c7a
Desconocido <quijadajosed@gmail.com>
2024-09-19 22:50:19 -0300

    add storage module with service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VideoConversionModule } from './video-conversion/video-conversion.module';
+import { StorageModule } from './storage/storage.module';
 
 @Module({
-  imports: [VideoConversionModule],
+  imports: [VideoConversionModule, StorageModule],
   controllers: [AppController],
   providers: [AppService],
 })
